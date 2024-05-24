d4fc950d67553dc0ade252fd07faf878e6770245
Medvezo <mansur.mustafayev@gmail.com>
2024-05-24 13:22:28 +0400

    feat: mapping cards with links in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <main>
-      Home page
-    </main>
-  );
+export default function Page() {
+	let cards = Array.from({ length: 6 }, (_, i) => i + 1);
+
+	return (
+		<section className="grid grid-cols-3 gap-4 justify-center items-center p-4 max-w-2xl mx-auto">
+			{cards.map((id) => (
+				<Link
+					className="flex max-sm:w-4/5 justify-center items-center h-48 bg-violet-300 rounded-lg text-black font-bold text-lg max-w-48"
+					key={id}
+					href={`/cards/${id}`}
+					passHref>
+					{id}
+				</Link>
+			))}
+		</section>
+	);
 }
