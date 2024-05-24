9d526d04522d4c063101ec16b8999d408ad31d11
Medvezo <mansur.mustafayev@gmail.com>
2024-05-24 13:27:22 +0400

    refactor: add props place in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Advanced Modal",
-  description: "Testing Parallel and Intercepting routes of Next.js by sample advanced routing modal",
+	title: "Advanced Modal",
+	description:
+		"Testing Parallel and Intercepting routes of Next.js by sample advanced routing modal",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout(props: {
+	children: React.ReactNode;
+	modal: React.ReactNode;
+}) {
+	return (
+		<html>
+			<body className={inter.className}>
+				{props.children}
+				{props.modal}
+				<div id="modal-root" />
+			</body>
+		</html>
+	);
 }
